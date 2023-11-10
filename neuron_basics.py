8312a51ab3294ba31dd377ba18e606d2f64388df
Anilabhimanyu <anilkumarreddy3418@gmail.com>
2023-11-10 14:19:00 +0530

    code for making code simpler, i.e, using for loop

--- neuron_basics.py
+++ neuron_basics.py
@@ -1,16 +1,24 @@
 import numpy as np
 
 inputs=[1,2,3]
-weights1=[0.2,0.3,0.4]
-weights2=[0.3,0.4,0.9]
-weights3=[0.1,0.3,0.8]
 
-bias=3
+weights_list=[
+    [0.2,0.3,0.4],
+    [0.3,0.4,0.9],
+    [0.1,0.3,0.8]
+]
+bias=[3,4,5]
+output_list=[]
 
-output=np.dot(inputs,weights1)+bias
-outputs=[inputs[0]*weights1[0]+inputs[1]*weights1[1]+inputs[2]*weights1[2]+bias,
-inputs[0]*weights2[0]+inputs[1]*weights2[1]+inputs[2]*weights2[2]+bias,
-inputs[0]*weights3[0]+inputs[1]*weights3[1]+inputs[2]*weights3[2]+bias]
+print(zip(weights_list,bias))
+for each_weights,each_bias in zip(weights_list,bias):
+    output_of_each_neuron=0
+    for i,j in zip(inputs,each_weights):
+        output_of_each_neuron+=i*j
+    output_of_each_neuron+=each_bias
+    output_list.append(output_of_each_neuron)
+print("output_list  ",output_list)
 
-print(output)
-print(outputs)
+    
+
+        
