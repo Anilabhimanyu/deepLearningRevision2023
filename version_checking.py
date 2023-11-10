81c1cdf31fce44259df9e622c312a6f1e5c1b539
anil <anilkumar@divum.in>
2023-11-10 10:52:45 +0530

    version checking in first commit

---
+++ version_checking.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib as plt
+import sys
+
+print("python version is ",sys.version)
+print("matplotlib version is ",plt.__version__)
+print("numpy version is ",np.__version__)
